d97c51d4490cdf767a96b6447304528687ba2b2f
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2023-06-15 17:06:34 +0900

    not to install tflite

--- eff_word_net/__init__.py
+++ eff_word_net/__init__.py
@@ -3,9 +3,11 @@
 """
 
 import os
-RATE=16000
-samples_loc = os.path.join(os.path.dirname(os.path.realpath(__file__)),"sample_refs")
 
-from eff_word_net.package_installation_scripts import check_install_tflite
+RATE = 16000
+samples_loc = os.path.join(os.path.dirname(os.path.realpath(__file__)), "sample_refs")
 
-check_install_tflite()
+# NOTE(kan-bayashi): Disable TFLite installation
+# from eff_word_net.package_installation_scripts import check_install_tflite
+#
+# check_install_tflite()
